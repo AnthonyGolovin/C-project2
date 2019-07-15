c22583aec49063632d73a988be986b5d02f95208
Student <example@example.com>
2019-07-15 14:16:41 -0700

    Initial commit with begining of while loop

---
+++ C#.cs
@@ -0,0 +1,66 @@
+// using System;
+// using System.Collections.Generic;
+
+// class Riddle
+// {
+//     public static Dictionary<string, string> differentRiddles = new Dictionary<string, string>();
+
+//     static void Randomizer()
+//     {
+//         Riddle2 rt = new Riddle2();
+//         rt.value = 2;
+//         Console.WriteLine(rt.value);
+//     }
+//   static void Main()
+//   {
+//       Console.WriteLine("Solve the Spinx's riddle and you may live");
+//       Console.WriteLine("You own a one story house with a car and kids in a world where everything that exists is red. All that exists in your house is red except the stairs, why?");
+//       string answer = Console.ReadLine();
+//       if (answer == "There are no stairs") {
+//           Console.WriteLine("Congrats you have solved the riddle");
+//           Riddle2();
+//       } else {
+//           Console.WriteLine("You have answered incorrectly, and as a result you have been eaten");
+//       }
+//   }
+//   static void Riddle2()
+//   {
+//       Console.WriteLine("Hmmm, heres a good riddle");
+//       Console.WriteLine("Duck....duck.....duck?");
+//       string answer = Console.ReadLine();
+//       if (answer == "Goose") {
+//           Console.WriteLine("You solved it, but I am unimpressed");
+//         //   Riddle3();
+//       } else {
+//           Console.WriteLine("You have answered incorrectly, and as a result you have been eaten");
+//       }
+//   }
+// //   static void Riddle3()
+// //   {
+// //       Console.WriteLine("Psst kid, want some riddles?");
+// //       Console.WriteLine("How much could a wood chuck chuck if a wood chuck could chuck wood");
+// //       string answer = Console.ReadLine();
+// //       if (answer == "About 7") {
+// //           Console.WriteLine("You cheated, niceeeeee");
+// //       } else {
+// //           Console.WriteLine("You have answered incorrectly, and as a result you have been eaten");
+// //       }
+// //   }
+// }
+using System;
+using System.Collections.Generic;
+
+class Riddle
+{
+    public static Dictionary<string, string> differentRiddles = new Dictionary<string, string>();
+
+    static void Randomizer()
+    {
+       
+    }
+  static void Main()
+  {
+  
+  }
+
+}
